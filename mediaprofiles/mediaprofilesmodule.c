e04bf989db4c2ae14659af868a7707e0d6efeb57
Raphael Slinckx <rslinckx@cvs.gnome.org>
2005-08-09 00:29:48 +0000

    Add python bindings for the gnome-media's gnome-media-profiles package.
    2005-08-09  Raphael Slinckx  <rslinckx@cvs.gnome.org>
    
    	* AUTHORS:
    	* Makefile.am:
    	* configure.ac:
    	* examples/mediaprofiles/profiles.py:
    	* mediaprofiles/.cvsignore:
    	* mediaprofiles/Makefile.am:
    	* mediaprofiles/mediaprofiles.defs:
    	* mediaprofiles/mediaprofiles.override:
    	* mediaprofiles/mediaprofilesmodule.c:
    
    	Add python bindings for the gnome-media's
    	gnome-media-profiles package. This allows to retreive
    	audio compression preferences from gconf, used for example
    	by Sound Juicer. See example file for usage.

---
+++ mediaprofiles/mediaprofilesmodule.c
@@ -0,0 +1,32 @@
+/* -*- Mode: C; c-basic-offset: 4 -*- */
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <pygobject.h>
+#include <pygtk/pygtk.h>
+
+#include <gnome-media/profiles/gnome-media-profiles.h>
+
+void pymediaprofiles_register_classes(PyObject *d);
+extern PyMethodDef pymediaprofiles_functions[];
+
+DL_EXPORT (void)
+initmediaprofiles (void)
+{
+    PyObject *m, *d;
+    
+    /* TODO: We leak a ref to gconf client here, who cares.. */
+    gnome_media_profiles_init (gconf_client_get_default ());
+
+    m = Py_InitModule("mediaprofiles", pymediaprofiles_functions);
+    d = PyModule_GetDict(m);
+
+    init_pygtk();
+    init_pygobject();
+    
+    pymediaprofiles_register_classes(d);
+
+    if (PyErr_Occurred())
+        Py_FatalError("could not initialise module mediaprofiles");
+}
