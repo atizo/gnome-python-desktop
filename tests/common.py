5ad658b8b39c723280c3915768a75a82f8820d22
Gustavo J. A. M. Carneiro <gjc@src.gnome.org>
2006-01-20 17:47:34 +0000

    misc..

--- tests/common.py
+++ tests/common.py
@@ -24,12 +24,15 @@ for item in modules:
         module = item
         dirname = item
     sys.path.insert(0, os.path.join("..", dirname))
+    print "Trying to import module %s... " % (module,),
     try:
         __import__(module) # try to import the module to catch undefined symbols
     except ImportError, ex:
         if ex.args[0].startswith("No module named"):
-            pass
+            print "not found"
         else:
             raise
+    else:
+        print "ok."
 
 ltihooks.uninstall()
